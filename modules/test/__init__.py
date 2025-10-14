0feb62591344fd5d24afaa752359cbe5b386693d
default2368 <default.mcp@gmail.com>
2025-10-14 12:15:15 +0200

    Create __init__.py

---
+++ modules/test/__init__.py
@@ -0,0 +1,4 @@
+from .test_functions import esegui_test_completo, saluta, somma_semplice
+
+# Questo rende le funzioni disponibili quando si importa il package "test"
+__all__ = ['esegui_test_completo', 'saluta', 'somma_semplice']
