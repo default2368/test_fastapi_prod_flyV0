44839ca2d680780eaa9662984194a6abee7ccb4b
default2368 <default.mcp@gmail.com>
2025-10-14 09:48:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,3 @@ async def welcome():
 @app.get("/")
 async def root():
     return {"info": "Vai su /welcome per il messaggio di benvenuto"}
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8080)
