9234d78102bb3234cffcb9c77e185b384f478df5
default2368 <default.mcp@gmail.com>
2025-10-14 12:55:12 +0200

    Delete test directory

--- test/__init__.py
+++
@@ -1,4 +0,0 @@
-from .test_functions import esegui_test_completo, saluta, somma_semplice
-
-# Questo rende le funzioni disponibili quando si importa il package "test"
-__all__ = ['esegui_test_completo', 'saluta', 'somma_semplice']
